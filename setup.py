1c17ddd82a26bd3247066c206bd66a599412bd55
Andrey Canon <andrey.canon@edunext.co>
2019-09-16 15:30:17 -0500

    Install requirements

--- setup.py
+++ setup.py
@@ -26,6 +26,39 @@ def get_version(*file_paths):
 VERSION = get_version('openedx_proversity_notifications', '__init__.py')
 
 
+def load_requirements(*requirements_paths):
+    """
+    Load all requirements from the specified requirements files.
+    Returns a list of requirement strings.
+    """
+    requirements = set()
+    for path in requirements_paths:
+        requirements.update(
+            line.split('#')[0].strip() for line in open(path).readlines()
+            if is_requirement(line)
+        )
+    return list(requirements)
+
+
+def is_requirement(line):
+    """
+    Return True if the requirement line is a package requirement;
+    that is, it is not blank, a comment, or editable.
+    """
+    # Remove whitespace at the start/end of the line
+    line = line.strip()
+
+    # Skip blank lines, comments, and editable installs
+    return not (
+        line == '' or
+        line.startswith('-r') or
+        line.startswith('#') or
+        line.startswith('-e') or
+        line.startswith('git+') or
+        line.startswith('-c')
+    )
+
+
 setup(
     name='openedx-proversity-notifications',
     version=VERSION,
@@ -36,7 +69,7 @@ setup(
         'openedx_proversity_notifications'
     ],
     include_package_data=True,
-    install_requires=[],
+    install_requires=load_requirements('requirements/base.in'),
     zip_safe=False,
     entry_points={
         "lms.djangoapp": [
