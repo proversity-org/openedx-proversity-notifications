7fc25936b7cefd73d0be6a6a96481df37944ac63
Andrey Canon <andrey.canon@edunext.co>
2019-09-16 14:12:29 -0500

    remove unused methods

--- openedx_proversity_notifications/edxapp_wrapper/backends/student_g_v1.py
+++ openedx_proversity_notifications/edxapp_wrapper/backends/student_g_v1.py
@@ -1,7 +1,7 @@
 """ Backend abstraction """
 from student.auth import user_has_role
 from student.roles import CourseStaffRole
-from student.models import CourseAccessRole, CourseEnrollment, UserProfile, user_by_anonymous_id
+from student.models import CourseEnrollment, user_by_anonymous_id
 
 
 def user_has_role_backend(*args, **kwargs):
@@ -12,16 +12,6 @@ def user_has_role_backend(*args, **kwargs):
 def course_staff_role_backend(*args, **kwargs):
     """ Real backend to get course staff role. """
     return CourseStaffRole(*args, **kwargs)
-
-
-def course_access_role():
-    """ Returns CourseAccessRole model. """
-    return CourseAccessRole
-
-
-def user_profile():
-    """ Returns UserProfile model. """
-    return UserProfile
 
 
 def course_enrollment():
--- openedx_proversity_notifications/edxapp_wrapper/get_student_library.py
+++ openedx_proversity_notifications/edxapp_wrapper/get_student_library.py
@@ -21,24 +21,6 @@ def get_course_staff_role(*args, **kwargs):
     return backend.course_staff_role_backend(*args, **kwargs)
 
 
-def course_access_role():
-    """ Get CourseAccessRole model. """
-
-    backend_function = settings.OPN_STUDENT_LIBRARY
-    backend = import_module(backend_function)
-
-    return backend.course_access_role()
-
-
-def get_user_profile():
-    """ Get UserProfile model. """
-
-    backend_function = settings.OPN_STUDENT_LIBRARY
-    backend = import_module(backend_function)
-
-    return backend.user_profile()
-
-
 def get_user_by_anonymous_id(*args, **kwargs):
     """ Returns the user_by_anonymous_id method. """
 
